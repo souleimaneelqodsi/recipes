ee39404bba560d62b4fd57de27a2574fce528b95
ZEROAUL_Mohammed <zeroual.md1@gmail.com>
2025-04-19 00:59:53 +0200

    Chanrgement des recette avec js

--- js/script.js
+++ js/script.js
@@ -50,3 +50,40 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+//chargement des recetttes
+function chargerRecettes() {
+    fetch('api/recipes') 
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementById('midou');
+        container.innerHTML = ''; 
+  
+        data.forEach(recette => {
+          const col = document.createElement('div');
+          col.className = 'col-lg-4 col-md-4 col-sm-6';
+  
+          col.innerHTML = `
+            <a href="recette.php?name=${encodeURIComponent(recette.nameFR)}" class="fh5co-card-item">
+                <figure>
+                <div class="overlay"><i class="ti-plus"></i></div>
+                <img src="${recette.imageURL}" alt="Image" class="img-responsive" width="400" height="300">
+                </figure>
+                <div class="fh5co-text">
+                <h2 style="font-size: 20px;">${recette.nameFR}</h2>
+                <p><strong>Auteur :</strong> ${recette.Author}</p>
+                <p><span class="price cursive-font">♡</span></p>
+                </div>
+            </a>
+            `;
+
+  
+          container.appendChild(col);
+        });
+      })
+      .catch(error => console.error('Erreur lors du chargement des recettes :', error));
+  }
+  
+  window.addEventListener('DOMContentLoaded', chargerRecettes);
+  
